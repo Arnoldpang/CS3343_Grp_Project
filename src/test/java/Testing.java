59cd8be40f5432dcc5338cbfe75ac4d514576b6f
Arnold <tszhopang4-c@my.cityu.edu.hk>
2025-10-04 17:49:49 +0800

    Initial commit

---
+++ src/test/java/Testing.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Testing {
+    @BeforeEach
+    public void setUp() throws Exception{
+
+    }
+
+}
